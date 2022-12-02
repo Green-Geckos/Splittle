b65b2965f10ff30342fd6c0afd887b6ced6c7380
Shaleen Bansal <bansal.shaleen@gmail.com>
2022-12-03 02:40:02 +0530

    handler changes

--- app/handler.js
+++ app/handler.js
@@ -2,8 +2,12 @@ import {Group}  from './objects/Group.js';
 import {User} from './objects/User.js';
 import {Expense} from './objects/Expense';
 
-
-function addGroupHandler(createdBy){
+/**
+ * 
+ * @param {address} createdBy -> User Address
+ * @param {string} groupName -> group Name given by user
+ * @param {address[]} members  -> list of user address in the group
+ */
+function createGroupHandler(createdBy, groupName, members){
     const group = new Group();
-
 }
--- app/objects/User.js
+++ app/objects/User.js
@@ -1,6 +1,5 @@
 export class User{
     constructor(address, name){
-        //this.userId = userId;
         this.address = address;
         this.name = name;
     }
