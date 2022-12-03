7deedd1474e350f0ea6dde90f72446e7adc584b2
Ganesh Tiwari <ganesht049@gmail.com>
2022-12-03 12:13:15 +0530

    feat: add form handling on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { useState, useEffect, use } from 'react'
 import { useRouter } from 'next/router'
-import { Box, Flex, Button, Input, Text, FormControl, FormLabel, FormHelperText } from '@chakra-ui/react'
+import { Box, Flex, Button, Input, Text, FormControl, FormLabel, FormHelperText, FormErrorMessage } from '@chakra-ui/react'
 
 import { ethers } from 'ethers'
+import { useForm } from 'react-hook-form';
 import EthersNotFound from '../components/AlertBoxes/EthersNotFound'
 
 import '@fontsource/poppins/700.css'
@@ -11,6 +12,11 @@ export default function Index() {
   const [name, setName] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [signer, setSigner] = useState();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
 
   const router = useRouter()
 
@@ -46,12 +52,12 @@ export default function Index() {
     }
   }
 
-  async function connectWallet(){
-    try{
+  async function connectWallet() {
+    try {
       const sign_result = await signer.signMessage("Sign message to confirm");
       console.log(sign_result);
     }
-    catch{
+    catch {
       console.log('Signing message failed');
     }
   }
@@ -63,27 +69,44 @@ export default function Index() {
         <Box w='100%' h='80px' maxH={'60px'} bg='brand.purple' >
 
         </Box>
-        <Flex justifyContent={'center'} alignItems='center' h='100%' w='100%' >
-          <Flex justifyContent={'center'} alignItems='center' w='80%' h='30vh' maxW='350px' >
-            <FormControl>
-              <Flex flexDir={'column'} w='100%'>
-                <FormLabel mb='8px'>Name:</FormLabel>
-                <Input
-                  value={name}
-                  onChange={handleChange}
-                  placeholder='Your name'
-                  required={true}
-                  size='md'
-                />
-                <FormHelperText fontSize={'xs'}>This is like a nick name stored with us for this address</FormHelperText>
-                <Button onClick={handleConnectWallet} colorScheme='teal' w='100%' size='md' mt={4} >
-                  Connect Wallet
-                </Button>
-              </Flex>
-            </FormControl>
+        <form onSubmit={handleSubmit(() => {})}>
+          <Flex justifyContent={'center'} flexDir='column' alignItems='center' h='100%' w='100%' >
+            <Flex flexDir={'column'} alignItems='center' w='80%' h='30vh' maxW='350px' >
+              <FormControl isInvalid={errors.name}>
+                <Flex flexDir={'column'} w='100%'>
+                  <FormLabel htmlFor='name' mb='8px'>Name:</FormLabel>
+                  <Input
+                    id='name'
+                    placeholder= 'Your name'
+                    {...register('name', {
+                      required: 'This is required',
+                      minLength: { value: 4, message: 'Minimum length should be 4' },
+                    })}
+                    size='md'
+                  />
+                  <FormHelperText
+                    fontSize={'xs'}>
+                    This is like a nick name stored with us for this address
+                  </FormHelperText>
+                  <FormErrorMessage>
+                    {errors.name && errors.name.message}
+                  </FormErrorMessage>
+                </Flex>
+              </FormControl>
+              <Button
+                onClick={handleConnectWallet}
+                colorScheme='teal'
+                w='100%'
+                size='md'
+                type='submit'
+                mt={4} >
+                Connect Wallet
+              </Button>
+            </Flex>
+
           </Flex>
 
-        </Flex>
+        </form>
 
       </Flex>
     </>
